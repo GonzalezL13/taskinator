3f35ace43ef3a82aa217db808751b614394de005
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-06-16 14:53:22 -0700

    created form to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEL = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-    
+var createTaskHandler = function() {   
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
